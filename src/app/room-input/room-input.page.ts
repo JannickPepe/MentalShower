7ececd6045dd5ba6a533151de4405c98b9d685d1
RukshanPokharel <rukshanpokharel@gmail.com>
2022-04-08 00:40:22 +0200

    changed bg color of gender and added buttons on grid

--- src/app/room-input/room-input.page.ts
+++ src/app/room-input/room-input.page.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-input.page.scss'],
 })
 export class RoomInputPage implements OnInit {
+  public zoneno: string;
+  public colorvar;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  btnClicked(zone) {
+    this.zoneno = zone;
   }
-
+  test() {
+    this.colorvar = document.getElementById('bgcolor').style.backgroundColor =
+      '#99D2FC';
+    this.colorvar = document.getElementById('bgcolor2').style.backgroundColor =
+      'rgba(153, 210, 252, 0.6)';
+  }
+  test1() {
+    this.colorvar = document.getElementById('bgcolor2').style.backgroundColor =
+      '#99D2FC';
+    this.colorvar = document.getElementById('bgcolor').style.backgroundColor =
+      'rgba(153, 210, 252, 0.6)';
+  }
 }
