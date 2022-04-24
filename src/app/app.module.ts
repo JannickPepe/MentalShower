56ed31781d10a29b3f6306fbd9e0fa49dbfe4247
Botond Horvath <hello@boti7.com>
2022-04-24 12:41:16 +0200

    Install NgRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
